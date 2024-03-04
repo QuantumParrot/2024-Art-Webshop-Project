0b2f639c7b06960005ff33defe0d6d5dd56b9161
QuantumParrot <ataraxia528491@gmail.com>
2024-03-05 04:05:01 +0800

    調整消息詳細頁面的版型

--- src/components/FrontFooter.vue
+++ src/components/FrontFooter.vue
@@ -1,18 +1,18 @@
 <template>
 
 <footer class="mt-auto bg-primary text-light">
-    <div class="container py-4">
-        <div class="flex-classic">
-            <h2 class="font-dancing mb-0">The Global Collector</h2>
-            <a class="fs-2" target="_blank" href="https://github.com/QuantumParrot/2024-Art-Webshop-Project">
-            <i class="bi bi-github"></i>
-            </a>
-        </div>
+<div class="container py-4">
+    <div class="flex-classic">
+        <h2 class="font-dancing mb-0">The Global Collector</h2>
+        <a class="fs-2" target="_blank" href="https://github.com/QuantumParrot/2024-Art-Webshop-Project">
+        <i class="bi bi-github"></i>
+        </a>
     </div>
-    <div class="bg-dark py-2 text-white text-center fs-7">
-        <i class="bi bi-c-circle me-2"></i>
-        <span>2024 The Global Collector Studio｜Non-Commercial Side Project</span>
-    </div>
+</div>
+<div class="bg-dark py-2 text-white text-center fs-7">
+    <i class="bi bi-c-circle me-2"></i>
+    <span>2024 The Global Collector Studio｜Non-Commercial Side Project</span>
+</div>
 </footer>
 
 </template>
--- src/views/front/ArticlesView.vue
+++ src/views/front/ArticlesView.vue
@@ -43,7 +43,7 @@
             </li>
             </template>
         </ul>
-        <div class="alert text-center py-7 mb-0" v-else>這個分類目前沒有文章喔！</div>
+        <div class="alert text-center py-7 mb-5" v-else>這個分類目前沒有文章喔！</div>
         <div class="d-flex justify-content-center" v-if="totalPages.column">
         <pagination-component
             :current="currentPage.column" :total="totalPages.column"

817368c94310bc3282c4c2ef67c24c24c7221adb
QuantumParrot <ataraxia528491@gmail.com>
2024-05-20 23:49:12 +0800

    feat: NotFound - 新增圖片載入前後的 transition 效果

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -5,24 +5,30 @@
     <p class="mb-7">
     請稍候，{{ count }} 秒後將跳轉回上一頁，您也可以<a href="#" class="text-info" @click.prevent="go">直接跳轉</a>
     </p>
-    <div class="rembrandt" v-show="!isTransition">
-        <img
-            src="https://storage.googleapis.com/vue-course-api.appspot.com/ataraxia/1709020880637.jpeg?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=RlRKLG%2F06bJtMDwvCxBpB91tS1DE5Gg9%2FLyUu9irrnJGRlK9SlrSgoIs6Oya6JEbIH9sJ0yGJbBL97DODf7ZdQ3VZ5F9iOf%2FGZyq7qvQFQFh6%2F3ut8fF4Cy9w31eMyf5jdZb8oocqwCqjlHDar9NKEUzQ7Jlib%2Bpc2%2Bhvi664V%2BJROJYj0SRdU1hzY0KAaEER8mzg7NHXOygcT2UPFsz0NcRfJvfPWa15gDbrydAUqszV35m0IYGqtqb%2BqFxHYBttT1NbiKvoM946eHhLpsh6X56FTX19P9diAo6G3Up2DUmKzf3O2vy7iy2tKkC2cbAt7JMVWUJMJ1%2FiDnaBDgMzA%3D%3D"
-            alt="Rembrandt's Portrait"
-            @load="onLoad">
-        <div class="img-description d-flex justify-content-center fs-7">
-            <div>
-            這是林布蘭於 1630 年繪製的自畫像，畫中的他雙眼大張，表情逗趣<br>
-            令人欽佩的是，這幅畫的原尺寸只有 5.1 cm × 4.5 cm 左右<br>
-            他在比郵票還小的紙張上描繪出如此生動的圖畫
+    <transition
+        enter-active-class="animate__animated animate__fadeIn"
+        leave-active-class="animate__animated animate__fadeIn">
+        <div class="rembrandt" v-show="!isTransition">
+            <img
+                src="https://storage.googleapis.com/vue-course-api.appspot.com/ataraxia/1709020880637.jpeg?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=RlRKLG%2F06bJtMDwvCxBpB91tS1DE5Gg9%2FLyUu9irrnJGRlK9SlrSgoIs6Oya6JEbIH9sJ0yGJbBL97DODf7ZdQ3VZ5F9iOf%2FGZyq7qvQFQFh6%2F3ut8fF4Cy9w31eMyf5jdZb8oocqwCqjlHDar9NKEUzQ7Jlib%2Bpc2%2Bhvi664V%2BJROJYj0SRdU1hzY0KAaEER8mzg7NHXOygcT2UPFsz0NcRfJvfPWa15gDbrydAUqszV35m0IYGqtqb%2BqFxHYBttT1NbiKvoM946eHhLpsh6X56FTX19P9diAo6G3Up2DUmKzf3O2vy7iy2tKkC2cbAt7JMVWUJMJ1%2FiDnaBDgMzA%3D%3D"
+                alt="Rembrandt's Portrait"
+                @load="onLoad">
+            <div class="img-description d-flex justify-content-center fs-7">
+                <div>
+                這是林布蘭於 1630 年繪製的自畫像，畫中的他雙眼大張，表情逗趣<br>
+                令人欽佩的是，這幅畫的原尺寸只有 5.1 cm × 4.5 cm 左右<br>
+                他在比郵票還小的紙張上描繪出如此生動的圖畫
+                </div>
             </div>
         </div>
-    </div>
+    </transition>
 </div>
 
 </template>
 
 <script>
+
+import 'animate.css';
 
 export default {
 
